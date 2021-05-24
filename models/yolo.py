24f8c45d38c245b7866aa352729fc58ec7948187
Thomas <tpfann@student.tudelft.nl>
2021-05-24 16:56:24 +0200

    added data format

--- models/yolo.py
+++ models/yolo.py
@@ -21,3 +21,11 @@ class Yolo(Model):
             inputs = [inputs]
         results = self.model(inputs)
         return results
+
+# results.xyxy[0]  # img1 predictions (tensor)
+# results.pandas().xyxy[0]  # img1 predictions (pandas)
+#      xmin    ymin    xmax   ymax  confidence  class    name
+# 0  749.50   43.50  1148.0  704.5    0.874023      0  person
+# 1  433.50  433.50   517.5  714.5    0.687988     27     tie
+# 2  114.75  195.75  1095.0  708.0    0.624512      0  person
+# 3  986.00  304.00  1028.0  420.0    0.286865     27     tie
